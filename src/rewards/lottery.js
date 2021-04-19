b1536902c4637c0583f00781047c675cb7093f10
Nikita Deriglazov <n.deriglazov@itransition.com>
2021-04-19 14:09:26 +0300

    print username in failed lottery message

--- src/rewards/lottery.js
+++ src/rewards/lottery.js
@@ -4,20 +4,21 @@ const messageHelper = require('../helpers/messageHelper');
 async function onLotteryCommand(channel, tags, message) {
   const valueForWin = 69;
   const userCustomValue = message;
+  const username = tags.username;
 
   const result = [
-    `/me @${tags.username} участвует в лотерее с числом "${userCustomValue}", но надеется на "${valueForWin}". Бот генерирует рандомное число...`,
+    `/me @${username} участвует в лотерее с числом "${userCustomValue}", но надеется на "${valueForWin}". Бот генерирует рандомное число...`,
   ];
 
   const generatedValue = numberHelper.randomInteger(1, 100);
 
   if (generatedValue === valueForWin) {
-    result.push(`/me @${tags.username} невероятно везёт! Он выигрывает в лотерее! Выпавшее число ${generatedValue}`);
+    result.push(`/me @${username} невероятно везёт! Он выигрывает в лотерее! Выпавшее число ${generatedValue}`);
   } else {
     if (generatedValue == userCustomValue) {
-      result.push(`/me @${tags.username} угадал число! Это заслуживает уважения и денежного поощрения! Выпавшее число ${generatedValue}`);
+      result.push(`/me @${username} угадал число! Это заслуживает уважения и денежного поощрения! Выпавшее число ${generatedValue}`);
     } else {
-      result.push(`/me К сожалению, выпало число  ${generatedValue}. Повезёт в другой раз!`);
+      result.push(`/me @${username}, к сожалению, выпало число  ${generatedValue}. Повезёт в другой раз!`);
     }
   }
 
