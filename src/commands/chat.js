74c5e9c9b837cde8ad5f1a7df5e27e9d3674a4c4
Nikita Deriglazov <n.deriglazov@itransition.com>
2022-12-26 18:12:26 +0300

    Chatters tag for chat command

--- src/commands/chat.js
+++ src/commands/chat.js
@@ -5,6 +5,7 @@ const API_URL = 'https://api.openai.com/v1/completions';
 
 async function onChatCommand(channel, tags, message) {
   let text = messageHelper.getSubjectFromMessage(message).trim();
+  const chatter = tags['display-name'] ?? tags.username;
 
   if (text.length === 0) {
     return 'Невозможно ответить на пустое сообщение';
@@ -29,10 +30,9 @@ async function onChatCommand(channel, tags, message) {
   });
 
   const answer = response?.data?.choices[0]?.text;
-  console.log(response?.data?.choices);
 
   if (typeof answer !== 'undefined') {
-    return answer;
+    return `@${chatter}, ${answer}`;
   }
 
   return 'Не удалось получить данные с OpenAI';
