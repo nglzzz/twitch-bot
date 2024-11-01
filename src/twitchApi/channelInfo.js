2b1bb357bc84ccac5194db08aaade3ef503ab3cd
Nikita Deriglazov <deriglazov.nikita@gmail.com>
2024-11-01 20:39:46 +0300

    Add copypasts

--- src/twitchApi/channelInfo.js
+++ src/twitchApi/channelInfo.js
@@ -2,9 +2,10 @@ const axios = require('axios');
 const config = require('../config');
 const getOAuthToken = require('./oauth');
 
-const getChannelInfo = async () => {
+const getChannelInfo = async (channel) => {
+  channel = channel || config.CHANNEL;
   const oauth = await getOAuthToken();
-  const response = await axios.get(`https://api.twitch.tv/helix/streams?user_login=${config.CHANNEL}`, {
+  const response = await axios.get(`https://api.twitch.tv/helix/streams?user_login=${channel}`, {
     headers: {
       'Authorization': `Bearer ${oauth.token}`,
       'Client-Id': config.TWITCH_API_CLIENT_ID,
