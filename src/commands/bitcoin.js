9a8fe3a34fdb54f13219eb2713c82da0939998cc
Nikita Deriglazov <deriglazov.nikita@gmail.com>
2025-03-12 21:45:51 +0300

    Fix bitcoin command

--- src/commands/bitcoin.js
+++ src/commands/bitcoin.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 async function onBitcoinCommand(channel, tags, message) {
   try {
-    const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json');
-    return 'Курс биткоина: $' + response.data.bpi.USD.rate;
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+    return 'Курс биткоина: $' + response.data.bitcoin.usd;
   } catch (error) {
     return 'Не удалось получить курс биткоина';
   }
