78e45dcc408c6e65312060db79348474a9caf34b
Nikita Deriglazov <deriglazov.nikita@gmail.com>
2025-06-06 21:37:25 +0300

    update specs

--- src/commands/specs.js
+++ src/commands/specs.js
@@ -1,8 +1,8 @@
 async function onSpecsCommand(channel, tags) {
   return 'Мой компьютер: ' +
-    'Процессор: AMD Ryzen 7 3700X; ' +
+    'Процессор: AMD Ryzen 7 9800X3D; ' +
     'Видеокарта: Nvidia RTX 4070 TI; ' +
-    'ОЗУ: 32 GB; ' +
+    'ОЗУ: 64 GB; ' +
     'Хранилище: SSD Samsung 860 Pro 256GB x2, SSD Samsung 870 Evo 1TB, SSD Intel 660p 2TB; ' +
     'Микрофон: Samson C01U Pro; ' +
     'Мониторы: Samsung Odyssey G5 32", AOC Q2775PQU';
