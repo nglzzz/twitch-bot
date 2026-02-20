9fd201b71c5cc981ad51351707bfbd44c9a45740
Nikita Deriglazov <deriglazov.nikita@gmail.com>
2026-02-20 20:38:53 +0300

    Donate commands updates

--- src/commands/donate.js
+++ src/commands/donate.js
@@ -1,7 +1,7 @@
 async function onDonateCommand(channel, tags) {
   const chatter = tags['display-name'] ?? tags.username;
 
-  return `@${chatter}, DonationAlerts - https://www.donationalerts.com/c/nglzzz StreamElements - https://streamelements.com/nglzzz/tip`;
+  return `@${chatter}, DonationAlerts - https://www.donationalerts.com/c/nglzzz DonateX - https://donatex.gg/donate/nglzzz StreamElements - https://streamelements.com/nglzzz/tip`;
 }
 
 module.exports = onDonateCommand;
--- src/commands/links.js
+++ src/commands/links.js
@@ -1,5 +1,6 @@
 async function onLinksCommand(channel, tags) {
-  return 'Донат: https://www.donationalerts.com/c/nglzzz | ' +
+  return 'DonationAlerts: https://www.donationalerts.com/c/nglzzz | ' +
+    'DonateX: https://donatex.gg/donate/nglzzz | ' +
     'Memealerts: https://memealerts.com/nglzzz | ' +
     'Telegram: https://t.me/nglzzzTV | ' +
     'Discord: https://discord.gg/uKCbdCGwTb | ' +
